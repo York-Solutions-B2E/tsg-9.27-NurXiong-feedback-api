acb76c5bff37f9eaec7240288223830d397fd836
AbdiNYork <AnurC@Yorksolutions.net>
2025-11-11 09:11:36 -0500

    Added Security filter chain: disabled CORS & CSRF No auth needed

---
+++ FeedbackApi/src/main/java/net/tsg_projects/feedbackapi/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,27 @@
+package net.tsg_projects.feedbackapi.SecurityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain  securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.
+                cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+
+                // Permit all incoming requests
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
+
+                //Disable basic auth and form login
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form.disable());
+        return http.build();
+    }
+}
